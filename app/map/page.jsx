2e6f17b63bb313f67fd0c2376e5eef75b8e50e80
Derek Westbrook <aralbayev.bek06@gmail.com>
2025-06-28 15:06:01 +0500

    Fix: added Map component

--- app/map/page.jsx
+++ app/map/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import 'leaflet/dist/leaflet.css';
 
 const Map = dynamic(() => import('@/components/Map'), {
   ssr: false,
--- components/Map.jsx
+++ components/Map.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
   return (
